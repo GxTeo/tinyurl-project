884b74533ab4ddc1be3cc8a7d820f59b2b3536c0
GxTeo <teog0015@e.ntu.edu.sg>
2024-10-26 19:14:19 +0800

    edit cors

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,15 +1,40 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
+	allowedOrigins := []string{
+		"http://localhost:80",
+		"http://example.com",
+		"http://anotherdomain.com",
+	}
+
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		// Get the origin from the request
+		origin := c.Request.Header.Get("Origin")
+
+		// Check if the origin is in the allowed origins
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+				break
+			}
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With")
+
+		// Handle preflight requests
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent) // Respond with 204 No Content
+			return
+		}
+
 		c.Next()
 	}
 }
